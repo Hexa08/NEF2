85c067cf53bc24b5521b16e94534dec0980d8ca5
Madhab  Dey <159428338+Hexa08@users.noreply.github.com>
2026-05-10 17:45:00 +0600

    Delete tests directory

--- tests/test_gpu_optional.py
+++
@@ -1,9 +0,0 @@
-def test_gpu_module_has_clear_availability_api():
-    from nef2 import gpu
-
-    assert callable(gpu.cuda_available)
-    assert callable(gpu.device_name)
-    assert callable(gpu.device_count)
-    assert callable(gpu.list_devices)
-    assert callable(gpu.set_device)
-    assert callable(gpu.use_device)
